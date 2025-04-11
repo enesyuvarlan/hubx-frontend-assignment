bd6b73fb85847f3ea3babe0c291e0f7a204e9e42
enesyuvarlan <nsnssoftware@gmail.com>
2025-04-11 13:37:02 +0300

    feat(color): add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: 'rgb(3, 129, 255)',
+        primaryHover: 'rgba(3, 129, 255, 0.2)',
+        border: 'rgba(0, 0, 0, 0.16)',
+        textDark: 'rgba(11, 23, 42, 1)',
+        background: 'rgba(245, 245, 247, 1)',
+        backgroundWhite: 'rgba(255, 255, 255, 1)',
+      },
       fontFamily: {
         sans: ['SF Pro Display', 'system-ui', 'sans-serif'],
       },
