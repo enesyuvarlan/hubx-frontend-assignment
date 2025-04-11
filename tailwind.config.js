8f500ea9d2bdec64d70aea8912f7f0794149d8b8
enesyuvarlan <nsnssoftware@gmail.com>
2025-04-11 12:57:04 +0300

    feat(fonts): add and configure SF-Pro-Display fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['SF Pro Display', 'system-ui', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        heavy: '800',
+      },
+      letterSpacing: {
+        '2px': '2px',
+        '0.8px': '0.8px',
+      },
+    },
   },
   plugins: [],
 }
