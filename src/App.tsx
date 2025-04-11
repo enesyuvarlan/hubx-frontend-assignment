57e5d91ee99ccdb34bc3100741e952a4d4bd9ad6
enesyuvarlan <nsnssoftware@gmail.com>
2025-04-11 12:48:39 +0300

    chore(setup): add Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import styles from './App.module.css';
-
 export function App() {
   return (
     <>
-      <header>
-        <h1>HubX Frontend Assignment</h1>
-      </header>
-
-      <main className={styles.main}></main>
+      <main></main>
     </>
   );
 }
