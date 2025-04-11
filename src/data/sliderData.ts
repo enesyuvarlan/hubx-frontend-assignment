b21c76d5c060f600cb339b6a7e600bc0084d144e
enesyuvarlan <nsnssoftware@gmail.com>
2025-04-11 13:52:00 +0300

    feat(data): add slider data

---
+++ src/data/sliderData.ts
@@ -0,0 +1,33 @@
+import DocumentIcon from '../assets/icons/document.svg';
+import CertifiedIcon from '../assets/icons/certified.svg';
+import ScannerIcon from '../assets/icons/scanner.svg';
+import FilterIcon from '../assets/icons/filter.svg';
+import ExportIcon from '../assets/icons/export.svg';
+
+export const sliderData = [
+  {
+    id: 0,
+    icon: DocumentIcon,
+    title: 'Document Scanner',
+  },
+  {
+    id: 1,
+    icon: CertifiedIcon,
+    title: 'Sign & Stamp',
+  },
+  {
+    id: 2,
+    icon: ScannerIcon,
+    title: 'Batch Scanning',
+  },
+  {
+    id: 3,
+    icon: FilterIcon,
+    title: 'Advanced Filters',
+  },
+  {
+    id: 4,
+    icon: ExportIcon,
+    title: 'Export & Share',
+  },
+];
