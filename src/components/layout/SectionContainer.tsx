5a429380c4c31425160526821fa6f77842a1d5ed
enesyuvarlan <nsnssoftware@gmail.com>
2025-04-12 03:14:56 +0300

    style(container): update padding and add background color

--- src/components/layout/SectionContainer.tsx
+++ src/components/layout/SectionContainer.tsx
@@ -3,7 +3,9 @@ import { ContainerProps } from '../../types/props';
 export const ContainerOuter = ({ children, ...props }: ContainerProps) => {
   return (
     <div {...props}>
-      <div className="mx-auto w-full mt-0 lg:mt-24">{children}</div>
+      <div className="mx-auto w-full mt-0 lg:mt-32  bg-backgroundWhite">
+        {children}
+      </div>
     </div>
   );
 };
@@ -11,7 +13,7 @@ export const ContainerOuter = ({ children, ...props }: ContainerProps) => {
 export const ContainerInner = ({ children, ...props }: ContainerProps) => {
   return (
     <div {...props}>
-      <div className="pt-14 lg:pt-[130px]">{children}</div>
+      <div className="pt-32">{children}</div>
     </div>
   );
 };
